c17781a7a17d52b56873a094c3b5e8b881e7c203
Oriol65 <150459853+Oriol65@users.noreply.github.com>
2025-12-16 17:25:44 +0000

    Started JS. All buttons log to console.

---
+++ javascript.js
@@ -0,0 +1,24 @@
+//Normal Numbers
+document.querySelector("#numberOne").onclick = () => console.log(1);
+document.querySelector("#numberTwo").onclick = () => console.log(2);
+document.querySelector("#numberThree").onclick = () => console.log(3);
+document.querySelector("#numberFour").onclick = () => console.log(4);
+document.querySelector("#numberFive").onclick = () => console.log(5);
+document.querySelector("#numberSix").onclick = () => console.log(6);
+document.querySelector("#numberSeven").onclick = () => console.log(7);
+document.querySelector("#numberEight").onclick = () => console.log(8);
+document.querySelector("#numberNine").onclick = () => console.log(9);
+document.querySelector("#numberZero").onclick = () => console.log(0);
+document.querySelector("#sign").onclick = () => console.log('*-1');
+document.querySelector("#period").onclick = () => console.log('.');
+//Operators
+document.querySelector("#divide").onclick = () => console.log('/');
+document.querySelector("#multiply").onclick = () => console.log('*');
+document.querySelector("#minus").onclick = () => console.log('-');
+document.querySelector("#plus").onclick = () => console.log('+');
+document.querySelector("#equal").onclick = () => console.log('=');
+//Special
+document.querySelector("#clear").onclick = () => console.log('AC');
+document.querySelector("#erase").onclick = () => console.log('C');
+document.querySelector("#percentage").onclick = () => console.log('%');
+
