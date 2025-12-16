58b33f2d72de36bd270427655d7df0411a886f3e
Oriol65 <150459853+Oriol65@users.noreply.github.com>
2025-12-16 20:04:12 +0000

    Main logic added, but needs refinement

--- javascript.js
+++ javascript.js
@@ -1,24 +1,98 @@
+const displayedNumber = document.querySelector("#displayedNumber")
+const oldDisplay= document.querySelector('#past')
 //Normal Numbers
-document.querySelector("#numberOne").onclick = () => console.log(1);
-document.querySelector("#numberTwo").onclick = () => console.log(2);
-document.querySelector("#numberThree").onclick = () => console.log(3);
-document.querySelector("#numberFour").onclick = () => console.log(4);
-document.querySelector("#numberFive").onclick = () => console.log(5);
-document.querySelector("#numberSix").onclick = () => console.log(6);
-document.querySelector("#numberSeven").onclick = () => console.log(7);
-document.querySelector("#numberEight").onclick = () => console.log(8);
-document.querySelector("#numberNine").onclick = () => console.log(9);
-document.querySelector("#numberZero").onclick = () => console.log(0);
-document.querySelector("#sign").onclick = () => console.log('*-1');
+document.querySelector("#numberOne").onclick = () => displayUpdate(first+='1');
+document.querySelector("#numberTwo").onclick = () => displayUpdate(first+='2');
+document.querySelector("#numberThree").onclick = () => displayUpdate(first+='3');
+document.querySelector("#numberFour").onclick = () => displayUpdate(first+='4');
+document.querySelector("#numberFive").onclick = () => displayUpdate(first+='5');
+document.querySelector("#numberSix").onclick = () => displayUpdate(first+='6');
+document.querySelector("#numberSeven").onclick = () => displayUpdate(first+='7');
+document.querySelector("#numberEight").onclick = () => displayUpdate(first+='8');
+document.querySelector("#numberNine").onclick = () => displayUpdate(first+='9');
+document.querySelector("#numberZero").onclick = () => displayUpdate(first+='0');
+document.querySelector("#sign").onclick = () => signChange();
 document.querySelector("#period").onclick = () => console.log('.');
 //Operators
-document.querySelector("#divide").onclick = () => console.log('/');
-document.querySelector("#multiply").onclick = () => console.log('*');
-document.querySelector("#minus").onclick = () => console.log('-');
-document.querySelector("#plus").onclick = () => console.log('+');
-document.querySelector("#equal").onclick = () => console.log('=');
+document.querySelector("#divide").onclick = () => calculate( sign ="/");
+document.querySelector("#multiply").onclick = () => calculate(sign='x');
+document.querySelector("#minus").onclick = () => calculate(sign='-');
+document.querySelector("#plus").onclick = () => calculate(sign='+');
+document.querySelector("#equal").onclick = () => calculate(sign='=');
 //Special
-document.querySelector("#clear").onclick = () => console.log('AC');
-document.querySelector("#erase").onclick = () => console.log('C');
+document.querySelector("#clear").onclick = () => clearAll();
+document.querySelector("#erase").onclick = () => displayUpdate(first = first.slice(0,-1));
 document.querySelector("#percentage").onclick = () => console.log('%');
+let first='';
+let second='';
+let lastSign=''
+let sign='';
 
+//Update display
+function displayUpdate(x=0){
+    if (x==''){
+        x=0;
+    }
+    displayedNumber.textContent = x;
+}
+function pastUpdate(x){
+    oldDisplay.textContent = x + ' '+ sign+'   ';
+}
+function clearAll(){
+    first='';
+    second='';
+    lastSign=''
+    sign='';
+    displayUpdate(first);
+    pastUpdate(0)
+}
+//Function to change the sign
+function signChange(){
+    first = (-1* parseFloat(first)).toString();
+    displayUpdate(first);
+}
+//Function to add a period, but only one allowed.. could potentially block or give a visual sign once used. 
+//Function to pass from number 1 to number 2
+function result(){
+    let secondOperand=0.0;
+    let firstOperand=0.0;
+    if (first!=''){
+        secondOperand=parseFloat(first);
+    }
+    if (second!=''){
+        firstOperand=parseFloat(second);
+    }
+    let total;
+    if (lastSign=="/"){
+        total=firstOperand/secondOperand;
+        console.log("it's dividing")
+        console.log(total)
+    } else if(lastSign=='x'){
+        total=firstOperand*secondOperand;
+        console.log("it's multiplying")
+        console.log(total)
+    } else if(lastSign=='-'){
+        total=firstOperand-secondOperand;
+        console.log("it's subtracting")
+        console.log(total)
+    } else if(lastSign=='+'){
+        total=firstOperand+secondOperand;
+        console.log("it's adding")
+        console.log(total)
+    } else {
+        total=firstOperand; //equal needs more work since it's not working correctly.
+    }
+    return(total.toString());
+}
+function calculate(sign){
+    if (second!=''){
+        first = result()
+    }
+    second=first;
+    first='';
+    lastSign=sign;
+    displayUpdate(second);
+    pastUpdate(second);
+}
+
+displayUpdate(first)
